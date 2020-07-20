848118a3a8aeb95b4dc99162d76525fd053ab49d
Ranadeep Dey <ranadeepdey01@gmail.com>
2020-07-21 01:14:47 +0530

    d

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,77 +1,135 @@
+function regsitrat() {
+  var x = document.getElementById("register");
+  var y = document.getElementById("tabless");
+  var z = document.getElementById("myCarousel");
+    
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+   
+}
 
+function regsitrations() {
+  var a = document.getElementById("tabless");
+  var b = document.getElementById("register");
+  var c = document.getElementById("myCarousel");
+
+   a.style.display = "block";
+   b.style.display = "none";
+    c .style.display = "none";
+}
   var rIndex,
   table = document.getElementById("table");
 
+ var today = new Date();
+ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+ var dateTime = date+' '+time;
+
 // check the empty input
-function checkEmptyInput() {
-  var isEmpty = false,
+function validation() {
+  var valid= true,
     name = document.getElementById("name").value,
-    id = document.getElementById("id").value,
-    qty = document.getElementById("quantity").value,
+    eid = document.getElementById("eid").value,
+    male = document.getElementById("male").value,
+    female = document.getElementById("female").value,
+    other= document.getElementById("other").value,
+    cno = document.getElementById("cno").value,
+    iid = document.getElementById("iid").value,
     category = document.getElementById("category"),
-    supplier = document.getElementById("supplier").value;
+    topic = document.getElementById("topic").value;
+
+    var atposition=eid.indexOf("@");  
+    var dotposition=eid.lastIndexOf("."); 
 
   if (name === "") {
-    alert("Product name field cannot be empty");
-    isEmpty = true;
-  } else if (id === "") {
-    alert("ID field cannot be empty");
-    isEmpty = true;
-  } else if (qty === "") {
-    alert("Quantity field cannot be empty");
-    isEmpty = true;
-  } else if (supplier === "") {
-    alert("Supplier field cannot be empty");
-    isEmpty = true;
+    alert("Name cannot be empty");
+    valid = false;
+  } else if (name.length<3) {
+    alert("Name too small");
+    valid = false;
+  } else if (name.length>30) {
+    alert("Name too big");
+    valid = false;
+  } else if (isNaN(name)==false){
+    alert("Name cannot be numeric");
+    valid=false;
+  } else if (eid === "") {
+    alert("Email id cannot be empty");
+    valid = false;
+  } else if (atposition<1 || dotposition<atposition+2 || dotposition+2>=eid.length) {
+    alert("Invalid Email id");
+    valid = false;
+  } else if (cno === "") {
+    alert("Contact No. cannot be empty");
+    valid = false;
+  } else if (isNaN(cno)==true) {
+    alert("Enter a Valid Phone Number (Only digits) ");
+    valid = false;
+  } else if (cno.length !=10 ) {
+    alert("Enter a Valid Phone Number (10 digit only)");
+    valid = false;
+  } else if (topic === "") {
+    alert("Topic cannot be empty");
+    valid = false;
   }
-  return isEmpty;
+
+
+  return valid;
 }
 
-//add a row
+
 function addHtmlTableRow() {
-  // get the table by id
-  // create a new row and cells
-  // get value from input text
-  // set the values into row cell's
+  
   var table = document.getElementById("table");
-  if (!checkEmptyInput()) {
+  if (validation()) {
+    alert("Form Submitted :)")
     var newRow = table.insertRow(table.length),
       cell1 = newRow.insertCell(0),
       cell2 = newRow.insertCell(1),
       cell3 = newRow.insertCell(2),
       cell4 = newRow.insertCell(3),
       cell5 = newRow.insertCell(4),
+      cell6 = newRow.insertCell(5),
+      cell7 = newRow.insertCell(6),
+      cell8 = newRow.insertCell(7);
 
-      name = document.getElementById("name").value,
-      id = document.getElementById("id").value,
-      qty = document.getElementById("quantity").value,
-      category = document.getElementById("category"),
-      supplier = document.getElementById("supplier").value;
+    name = document.getElementById("name").value,
+    eid = document.getElementById("eid").value,
+    male = document.getElementById("male").value,
+    female = document.getElementById("female").value,
+    other= document.getElementById("other").value,
+    cno = document.getElementById("cno").value,
+    iid = document.getElementById("iid").value,
+    
+    topic = document.getElementById("topic").value;
 
-    cell1.innerHTML = name;
-    cell2.innerHTML = id;
-    cell3.innerHTML = qty;
-    cell4.innerHTML = category;
-    cell5.innerHTML = supplier;
+    var e = document.getElementById("category");
+    var cat = e.options[e.selectedIndex].text;
+
+    if (document.getElementById('male').checked) {
+    g = document.getElementById('male').value;
+    }else if (document.getElementById('female').checked) {
+    g = document.getElementById('female').value;
+  } else if (document.getElementById('other').checked) {
+    g = document.getElementById('other').value;
+  }
+
+    cell1.innerHTML = dateTime;
+    cell2.innerHTML = name;
+    cell3.innerHTML = eid;
+    cell4.innerHTML = g;
+    cell5.innerHTML = cno;
+    cell6.innerHTML = iid;
+    cell7.innerHTML = cat;
+    cell8.innerHTML = topic;
     // call the function to set the event to the new row
     selectedRowToInput();
+    document.getElementById("reg").reset();
+    document.reg.reset();
   }
 }
 
-// display selected row data into input text
-function selectedRowToInput() {
 
-  for (var i = 1; i < table.rows.length; i++) {
-    table.rows[i].onclick = function() {
-      // get the seected row index
-      rIndex = this.rowIndex;
-      document.getElementById("name").value = this.cells[0].innerHTML;
-      document.getElementById("id").value = this.cells[1].innerHTML;
-      document.getElementById("quantity").value = this.cells[2].innerHTML;
-      document.getElementById("category").value = this.cells[3].innerHTML;
-      document.getElementById("supplier").value = this.cells[4].innerHTML;
-    };
-  }
-}
-selectedRowToInput();
  
+
